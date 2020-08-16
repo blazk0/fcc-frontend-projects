078cdfc20cc74b0f8d9b7ba9f2ed59b26802855d
Amr Abo Elenin <amr_aboelenin95@yahoo.com>
2020-08-16 16:28:23 +0300

    drum app

---
+++ drum-app/src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import DrumPad from './components/DrumPad';
+import data from './utils';
+
+function App() {
+  const [key, setKey] = useState<number>();
+  const [title, setTitle] = useState<string>('');
+
+  useEffect(() => {
+    document.addEventListener('keydown', e => setKey(e.keyCode));
+
+    document.addEventListener('keyup', () => setKey(0));
+
+    return () => {
+      document.removeEventListener('keydown', () => setKey(0));
+
+      document.addEventListener('keyup', () => setKey(0));
+    };
+  }, []);
+
+  return (
+    <div id="drum-machine">
+      <div id="display">
+        {data.map(drum => (
+          <DrumPad
+            drum={drum}
+            key={drum.id}
+            keyCode={key}
+            changeText={setTitle}
+          />
+        ))}
+      </div>
+
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ drum-app/src/components/DrumPad.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, useEffect } from 'react';
+import { Drum } from '../@types';
+
+interface Props {
+  drum: Drum;
+  keyCode: number | undefined;
+  changeText: (title: string) => void;
+}
+
+const DrumPad = ({ drum, keyCode, changeText }: Props) => {
+  const audio = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (keyCode === drum.keyCode) {
+      audio.current?.play();
+
+      changeText(drum.id);
+    }
+  }, [keyCode]);
+
+  return (
+    <div
+      className={`drum-pad ${keyCode === drum.keyCode && 'active'}`}
+      id={drum.id}
+      onClick={() => audio.current?.play()}
+    >
+      <audio id={drum.text} src={drum.sound} className="clip" ref={audio} />
+      {drum.text}
+    </div>
+  );
+};
+
+export default DrumPad;
